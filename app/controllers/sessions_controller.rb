78240d9a592c09285c35a3806875fca26a866306
imhilla <hillaryodhiambo282@gmail.com>
2020-11-11 17:46:29 +0300

    add login and logout session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  include CurrentUserConcern
   def create 
     user = User
     .find_by(email: params["user"]["email"])
@@ -17,6 +18,20 @@ class SessionsController < ApplicationController
   end
 
   def logged_in
-    
+    if @current_user
+       render json: {
+         logged_in: true,
+         user: @current_user
+       }
+      else 
+        render json: {
+          logged_in: false
+        }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: { status: 200, logged_out: true }
   end
 end
