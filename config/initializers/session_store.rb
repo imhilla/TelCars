49b6a231e8735c3b291a184fd12dc823c9b3bd75
imhilla <hillaryodhiambo282@gmail.com>
2020-11-11 17:51:38 +0300

    add if statement to session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
+if Rails.env == "production"
 Rails.application.config.session_store :cookie_store, key: '_booking_app', domain: 'booking'
+else 
+  Rails.application.config.session_store :cookie_store, key: '_booking_app'
+end
