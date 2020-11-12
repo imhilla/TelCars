a3cb7e12e2b12be440ebf0c15adc2bcc0bd7bc61
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 19:31:47 +0300

    import all welcome images

--- react-frontend/src/components/Welcome.js
+++ react-frontend/src/components/Welcome.js
@@ -1,6 +1,11 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Welcome1 from '../images/welcome1.jpg';
+import Welcome2 from '../images/welcome2.jpg';
+import Welcome3 from '../images/welcome3.jpg';
+import Welcome4 from '../images/welcome4.jpg';
 
 class Welcome extends React.Component {
   constructor(props) {
