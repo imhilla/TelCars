1fc915c8fa3f3dc0de9be599d1df6dcf8181b75d
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 14:13:28 +0300

    fix current user concern issue and user_id

--- rails-backend/app/controllers/sessions_controller.rb
+++ rails-backend/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     .find_by(email: params["user"]["email"])
     .try(:authenticate, params["user"]["password"])
     if user
-      session[:user_id] = user_id
+      session[:user_id] = user.id
       render json: {
         status: :created,
         logged_in: true,
