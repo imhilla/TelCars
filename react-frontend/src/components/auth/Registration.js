1c9f32d8c1e7c5be1f84619af11aec85d7bcff25
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 09:57:13 +0300

    add handle change events and constructor

--- react-frontend/src/components/auth/Registration.js
+++ react-frontend/src/components/auth/Registration.js
@@ -1,10 +1,36 @@
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    console.log('handle change', event);
+  }
+
+  handleSubmit(event) {
+    console.log('form submitted');
+    event.preventDefault();
+  }
+
   render() {
+    const email = this.state;
     return (
       <div>
-        Registration
+        <form onSubmit={this.handleSubmit}>
+          <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange} required />
+        </form>
       </div>
     );
   }
