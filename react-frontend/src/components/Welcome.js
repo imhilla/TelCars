cd7be139591870c4a19986f296c1e22a75fbf85f
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 19:27:17 +0300

    add more images

--- react-frontend/src/components/Welcome.js
+++ react-frontend/src/components/Welcome.js
@@ -1,7 +1,15 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nextImage: false,
+    };
+  }
+
   render() {
     return (
       <div>Hello world</div>
