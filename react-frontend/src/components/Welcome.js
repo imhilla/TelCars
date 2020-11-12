ada3a55dee121fa717bb7e7fa4ca557d4eb89067
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 19:50:59 +0300

    add slide container divs

--- react-frontend/src/components/Welcome.js
+++ react-frontend/src/components/Welcome.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
 import Welcome1 from '../images/welcome1.jpg';
 import Welcome2 from '../images/welcome2.jpg';
 import Welcome3 from '../images/welcome3.jpg';
@@ -26,7 +27,31 @@ class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <img src={slideImages[2]} alt="" />
+        {/* <img src={slideImages[2]} alt="" /> */}
+        <div className="slide-container">
+          <Slide>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                <span>Slide 1</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
+                <span>Slide 2</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+                <span>Slide 3</span>
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[3]})` }}>
+                <span>Slide 3</span>
+              </div>
+            </div>
+          </Slide>
+        </div>
       </div>
     );
   }
