0a34197599bc8fd3919dfd3869694a165d376519
imhilla <hillaryodhiambo282@gmail.com>
2020-11-12 19:47:57 +0300

    add slide image array

--- react-frontend/src/components/Welcome.js
+++ react-frontend/src/components/Welcome.js
@@ -2,10 +2,18 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import { Slide } from 'react-slideshow-image';
 import Welcome1 from '../images/welcome1.jpg';
 import Welcome2 from '../images/welcome2.jpg';
 import Welcome3 from '../images/welcome3.jpg';
 import Welcome4 from '../images/welcome4.jpg';
+
+const slideImages = [
+  Welcome1,
+  Welcome2,
+  Welcome3,
+  Welcome4,
+];
 
 class Welcome extends React.Component {
   constructor(props) {
@@ -18,7 +26,7 @@ class Welcome extends React.Component {
   render() {
     return (
       <div>
-        <img src={Welcome3} alt="" />
+        <img src={slideImages[2]} alt="" />
       </div>
     );
   }
