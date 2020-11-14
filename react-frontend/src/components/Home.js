122bfc5e2f7123b293bf73987b8b67fd7e244063
imhilla <hillaryodhiambo282@gmail.com>
2020-11-14 16:24:31 +0300

    install fontawesome

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -39,7 +39,7 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div className="home">
+      <div>
         <div className="homeContainer">
           <Logo />
           <NavBar />
