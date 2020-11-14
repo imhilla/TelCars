7f2b0761e8cdbddd10202bc9e1d022cebb77f977
imhilla <hillaryodhiambo282@gmail.com>
2020-11-14 14:08:56 +0300

    add few styles to navbar

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -3,15 +3,16 @@ import React from 'react';
 import {
   Link,
 } from 'react-router-dom';
+import './navbar.css';
 
 class NavBar extends React.Component {
   render() {
     return (
-      <div>
-        <Link to="/">HOME</Link>
-        <Link to="/lifestyle">LIFESTYLE</Link>
-        <Link to="/shop">SHOP</Link>
-        <Link to="/book">BOOK</Link>
+      <div className="nav">
+        <Link to="/" className="lii">HOME</Link>
+        <Link to="/lifestyle" className="lii">LIFESTYLE</Link>
+        <Link to="/shop" className="lii">SHOP</Link>
+        <Link to="/book" className="lii">BOOK</Link>
       </div>
     );
   }
