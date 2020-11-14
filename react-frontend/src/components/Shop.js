d6104e9cd6f6585e4b31ca74364d3c561f04e8bb
imhilla <hillaryodhiambo282@gmail.com>
2020-11-14 13:37:27 +0300

    rafactor navbar to all components

--- react-frontend/src/components/Shop.js
+++ react-frontend/src/components/Shop.js
@@ -1,10 +1,18 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import Logo from './Logo';
+import NavBar from './Navbar';
 
 class Shop extends React.Component {
   render() {
     return (
-      <div><h1>This is the latest</h1></div>
+      <div>
+        <div>
+          <Logo />
+          <NavBar />
+        </div>
+        <h1>This is the latest</h1>
+      </div>
     );
   }
 }
