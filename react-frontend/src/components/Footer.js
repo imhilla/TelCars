fc26128a67861fd0b83143e4f88f14fc869dd3f7
imhilla <hillaryodhiambo282@gmail.com>
2020-11-14 16:30:48 +0300

    add font awesome icons

--- react-frontend/src/components/Footer.js
+++ react-frontend/src/components/Footer.js
@@ -1,10 +1,19 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import { FaTwitter, FaFacebookF, FaGooglePlusG, FaPinterestP } from 'react-icons/fa';
 
 class Footer extends React.Component {
   render() {
     return (
-      <div>Footer</div>
+      <div>
+        <div>
+          <FaTwitter />
+          <FaFacebookF />
+          <FaGooglePlusG />
+          <FaPinterestP />
+        </div>
+        Footer
+      </div>
     );
   }
 }
