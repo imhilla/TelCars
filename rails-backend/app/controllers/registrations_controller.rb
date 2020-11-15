f6c7b624ec2fe7c39006a405d13f399ad0024443
imhilla <hillaryodhiambo282@gmail.com>
2020-11-15 08:40:59 +0300

    fix issue in registration controller

--- rails-backend/app/controllers/registrations_controller.rb
+++ rails-backend/app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < ApplicationController
     user = User.create!(
       email: params['user']['email'],
       password: params['user']['password'],
-      password_confirmation: params['user']['password_confirmation']
+      password_confirmation: params['user']['password_confirmation'],
       username: params['user']['username']
     )
     if user
