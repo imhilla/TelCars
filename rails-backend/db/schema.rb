0407662d805e5a854d39792816e908a305987ccb
imhilla <hillaryodhiambo282@gmail.com>
2020-11-16 11:28:23 +0300

    generate migration

--- rails-backend/db/schema.rb
+++ rails-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_064204) do
+ActiveRecord::Schema.define(version: 2020_11_16_082443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 2020_11_16_064204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "title"
+    t.string "body"
+    t.string "objectives"
+    t.string "services"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
