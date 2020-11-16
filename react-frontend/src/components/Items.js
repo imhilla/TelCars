6c5f1053d2801ac458aef3dc4bd2cba0e6f8d76d
imhilla <hillaryodhiambo282@gmail.com>
2020-11-16 15:16:42 +0300

    add items constructor

--- react-frontend/src/components/Items.js
+++ react-frontend/src/components/Items.js
@@ -1,7 +1,15 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
+import axios from 'axios';
 
 class Items extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
   render() {
     return (
       <div>Items component</div>
