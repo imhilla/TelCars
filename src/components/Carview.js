85329325562f07bc50fe12fcc3d53e0c548e2ac9
imhilla <hillaryodhiambo282@gmail.com>
2020-11-20 14:01:27 +0300

    get one car from state

--- src/components/Carview.js
+++ src/components/Carview.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import Logo from './Logo';
@@ -5,6 +6,20 @@ import Footer from './Footer';
 import NavBar from './Navbar';
 
 class Carview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const id = params.model_id;
+    console.log(id);
+  }
+
   render() {
     return (
       <div>
@@ -13,8 +28,11 @@ class Carview extends React.Component {
           <NavBar className="mynav" />
           <Footer className="footerr" />
         </div>
-        {console.log('yes')}
-        Hello world
+        <div className="descriptionContainer">
+          <h1>LATEST 2020 CAR MODELS</h1>
+          <h2>Please select our top lates cars of choice</h2>
+        </div>
+        <div />
       </div>
     );
   }
