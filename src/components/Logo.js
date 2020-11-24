54552e7004c6a4a57601c8c62bd08d4a99e67bdb
imhilla <hillaryodhiambo282@gmail.com>
2020-11-24 16:34:09 +0300

    fix logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import React from 'react';
 class Logo extends React.Component {
   render() {
     return (
-      <div><h1>Logo</h1></div>
+      <div className="logoo"><h1>TelaCars</h1></div>
     );
   }
 }
