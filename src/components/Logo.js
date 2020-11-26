c7d47fce3f9276bb094f9dbc5d7a2829a5b29a59
imhilla <hillaryodhiambo282@gmail.com>
2020-11-26 15:09:41 +0300

    use fuction component for the logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,7 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
-class Logo extends React.Component {
-  render() {
-    return (
-      <div className="logoo"><h1>TelaCars</h1></div>
-    );
-  }
+export default function Logo() {
+  return (
+    <div className="logoo"><h1>TelaCars</h1></div>
+  );
 }
-export default Logo;
