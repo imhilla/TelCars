6e7ad5c7a1985a6ab32b80dcc2ee4e7450d65fce
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 14:23:06 +0300

    use local host

--- src/components/setupProxy.js
+++ src/components/setupProxy.js
@@ -3,8 +3,8 @@ const { proxy } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     proxy('registrations', {
-      target: 'http://localhost:3000',
-      secure: false,
+      target: 'http://localhost:3001',
+      // secure: false,
       changeOrigin: true,
     }),
   );
