435101d6abc6a528d6ff9b32a51ea337afc38ea8
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 14:04:59 +0300

    use proxy middleware

--- src/components/setupProxy.js
+++ src/components/setupProxy.js
@@ -3,7 +3,7 @@ const { proxy } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     proxy('registrations', {
-      target: 'https://infinite-ocean-27248.herokuapp.com',
+      target: 'http://localhost:3000',
       secure: false,
       changeOrigin: true,
     }),
