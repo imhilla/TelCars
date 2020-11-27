75cd0f31049175a54f14fe4a328de58abaadf4e5
imhilla <hillaryodhiambo282@gmail.com>
2020-11-27 13:56:59 +0300

    use proxy middleware

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -42,7 +42,7 @@ class Registration extends React.Component {
     const {
       email, password, password_confirmation, username,
     } = this.state;
-    axios.post('https://infinite-ocean-27248.herokuapp.com/registrations', {
+    axios.post('/registrations', {
       user: {
         email,
         password,
