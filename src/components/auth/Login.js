a46fcfcca27e0e51d00ffb381eba8ff083fbbc0b
imhilla <hillaryodhiambo282@gmail.com>
2020-11-30 08:29:25 +0300

    add testing libraries

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,3 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-unused-state */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import PropTypes from 'prop-types';
