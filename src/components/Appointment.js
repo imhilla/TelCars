13b2c063f21f06911ba59498f41ea66479c9154f
imhilla <hillaryodhiambo282@gmail.com>
2020-12-01 11:33:23 +0300

    edit book appointment

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -78,14 +78,8 @@ export default function Appointment({ user, userId }) {
       <div className="hardline" />
       <div className="appointmentcontent">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus elit id tortor
-          pulvinar sollicitudin.
-          Nam placerat, risus vitae sollicitudin consequat, neque nunc
-          vulputate justo, ac volutpat nunc ex elementum enim.
-          Donec egestas vestibulum metus, in porttitor nulla facilisis id.
-          Vivamus fermentum vitae neque vel blandit.
-          Aenean ut mauris at arcu tincidunt vestibulum vel a mi.
-          Fusce eget ipsum vitae tortor sollicitudin rutrum
+          Please let us know what you would like to do so we can help meet your needs.
+          Book an appointment.
         </p>
       </div>
       <div className="booksection">
