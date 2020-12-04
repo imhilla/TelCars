679fb5901af84372c585430cd9d2e11682db7c5b
imhilla <hillaryodhiambo282@gmail.com>
2020-12-04 09:19:53 +0300

    add authentication request actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const login = login => ({ type: 'LOGIN', payload: login });
+export const authenticationRequest = request => ({ type: 'AUTHENTICATION_REQUEST', payload: request });
+export const authenticationSuccess = request => ({ type: 'AUTHENTICATION_SUCCESS', payload: request });
+export const authenticationFailure = request => ({ type: 'AUTHENTICATION_FAILURE', payload: request });
+export const logout = request => ({ type: 'LOGOUT', payload: request });
